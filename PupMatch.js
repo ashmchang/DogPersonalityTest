a9788837a996eb057e7a2a7ab991a9f29cb06654
ashmchang <ashleymchang27@gmail.com>
2015-12-17 15:47:23 -0800

    showing petfinder dog name

--- PupMatch.js
+++ PupMatch.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
 
-      var resultQ1 = function(){
+      // var resultQ1 = function(){
 
-            $("#q1").click("#results");
+      //       $("#q1").click("#results");
 
-            var personalityResults = $("input[name='q1']:checked").val();
-      }
+      //       var personalityResults = $("input[name='q1']:checked").val();
+      // }
       
       // still need to fix CSS, add the API + create a new variable for each dog to feed the api
       
@@ -37,82 +37,82 @@ $(document).ready(function(){
 
             if (finalresults === 'ENFP'){
                   $('#results').append( intro + finalresults +'<img src="http://imgs.sfgate.com/blogs/images/sfgate/pets/2010/06/11/border_collie3.jpg">' + '<a href="http://www.akc.org/dog-breeds/border-collie/">Border Collie</a> ' +'<p>Extremely energetic, smart, and affectionate.</p>');
-                  dogBreed = Border+Collie;
+                  dogBreed = 'Border+Collie';
                   };
 
             if (finalresults === 'ENFJ'){
                   $('#results').append( intro + finalresults +'<img src="http://petparent.me/wp-content/uploads/2014/05/13527-Sable-Shetland-Sheepdog-Sheltie-white-background.jpg">' + '<a href="http://www.akc.org/dog-breeds/shetland-sheepdog/">Shetland Sheepdog</a> ' +'<p>Bright, energetic and playful.  Loyal to the family, but reserved towards strangers.</p>');
-                  dogBreed = Shetland+Sheepdog+Sheltie;
+                  dogBreed = 'Shetland+Sheepdog+Sheltie';
                   };
 
             if (finalresults === 'ENTP'){
                   $('#results').append( intro + finalresults +'<img src="http://cf.ltkcdn.net/dogs/images/std/65325-371x324-Great_dane.jpg">' + '<a href="http://www.akc.org/dog-breeds/great-dane/">Great Dane</a> ' +'<p>Friendly, patient, and dependable.</p>');
-                  dogBreed = Great+Dane;
+                  dogBreed = 'Great+Dane';
                   };
 
             if (finalresults === 'ENTJ'){
                   $('#results').append( intro + finalresults +'<img src="http://www.rarebreed.com/breeds/kelpie/austrailiankelpie.jpg">' + '<a href="http://www.dogbreedinfo.com/australiankelpie.html">Austrialian Kelpie</a> ' +'<p>A compact, robust, and enthusiastic working dog.</p>');
-                  dogBreed = Australian+Kelpie;
+                  dogBreed = 'Australian+Kelpie';
                   };
 
             if (finalresults === 'ESFP'){
                   $('#results').append( intro + finalresults +'<img src="https://upload.wikimedia.org/wikipedia/en/0/07/Home_cocker_spaniel.jpg">' + '<a href="http://http://www.akc.org/dog-breeds/cocker-spaniel/">Cocker Spaniel</a> ' +'<p>Gentle, happy, and smart.</p>');
-                  dogBreed = Cocker+Spaniel;
+                  dogBreed = 'Cocker+Spaniel';
                   };
 
             if (finalresults === 'ESTJ'){
                   $('#results').append( intro + finalresults +'<img src="https://upload.wikimedia.org/wikipedia/commons/2/24/Toy_Fox_Terrier_2.jpg">' + '<a href="http://www.akc.org/dog-breeds/toy-fox-terrier/">Toy Fox Terrier</a> ' +'<p>Intelligent, Alert, Friendly</p>');
-                  dogBreed = Toy+Fox+Terrier;
+                  dogBreed = 'Toy+Fox+Terrier';
                   };
 
             if (finalresults === 'ESTP'){
                   $('#results').append( intro + finalresults +'<img src="http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Dalmatian-2.jpg">' + '<a href="http://www.akc.org/dog-breeds/dalmatian/">Dalmatian</a> ' +'<p>Outgoing, Diginified, Smart.</p>');
-                  dogBreed = Dalmatian;
+                  dogBreed = 'Dalmatian';
                   };
 
             if (finalresults === 'ESFJ'){
                   $('#results').append( intro + finalresults +'<img src="http://www.yourpurebredpuppy.com/dogbreeds/photos-AB/bullmastiffsf5.jpg">' + '<a href="http://www.akc.org/dog-breeds/bullmastiff/">Bullmastiff</a> ' +'<p>Brave, affectionate, and loyal.</p>');
-                  dogBreed = Bullmastiff;
+                  dogBreed = 'Bullmastiff';
                   };
 
             if (finalresults === 'INFP'){
                   $('#results').append( intro + finalresults +'<img src="http://www.trainpetdog.com/images2/bernesemountaindog4.jpg">' + '<a href="http://www.akc.org/dog-breeds/bernese-mountain-dog/">Bernese Mountain Dog</a> ' +'<p>Good-natured, calm, and strong.</p>');
-                  dogBreed = Bernese+Mountain+Dog;
+                  dogBreed = 'Bernese+Mountain+Dog';
                   };
 
             if (finalresults === 'INFJ'){
                   $('#results').append( intro + finalresults +'<img src="http://www.animal-photography.com/images/ailpeww4o.jpg">' + '<a href="http://www.akc.org/dog-breeds/collie">Collie</a> ' +'<p>Graceful, devoted, and proud.</p>');
-                  dogBreed = Collie;
+                  dogBreed = 'Collie';
                   };
 
             if (finalresults === 'INTP'){
                   $('#results').append( intro + finalresults +'<img src="https://s-media-cache-ak0.pinimg.com/236x/32/75/fb/3275fb17310c9a60ea7ad20e52d97606.jpg">' + '<a href="http://www.akc.org/dog-breeds/giant-schnauzer/">Giant Schnauzer</a> ' +'<p>Loyal, alert, trainable.</p>');
-                  dogBreed = Giant+Schnauzer;
+                  dogBreed = 'Giant+Schnauzer';
                   };
 
             if (finalresults === 'INTJ'){
                   $('#results').append( intro + finalresults +'<img src="http://cdn-www.dailypuppy.com/dog-images/bunny-the-dachshund-7_48531_2010-08-20_w450.jpg">' + '<a href="http://www.akc.org/dog-breeds/dachshund/">Dachshund</a> ' +'<p>Spunky, curious, friendly.</p>');
-                  dogBreed = Dachshund;
+                  dogBreed = 'Dachshund';
                   };
 
             if (finalresults === 'ISFP'){
                   $('#results').append( intro + finalresults +'<img src="https://s-media-cache-ak0.pinimg.com/236x/36/30/5f/36305f94f60c0df3caa868c4d73564b5.jpg">' + '<a href="http://www.akc.org/dog-breeds/labrador-retriever/">Labrador Retriever</a> ' +'<p>Friendly, active, outgoing.</p>');
-                  dogBreed = Labrador+Retriever;
+                  dogBreed = 'Labrador+Retriever';
                   };
 
             if (finalresults === 'ISTJ'){
                   $('#results').append( intro + finalresults +'<img src="hhttps://s.graphiq.com/sites/default/files/465/media/images/t2/Jack_Russell_Terrier_5687866.jpg">' + '<a href="http://www.dogbreedinfo.com/jackrussellterrier.htm">Jack Russell Terrier</a> ' +'<p>Cheerful, merry, and devoted.</p>');
-                  dogBreed = Jack+Russell+Terrier;
+                  dogBreed = 'Jack+Russell+Terrier';
                   };
 
             if (finalresults === 'ISTP'){
                   $('#results').append( intro + finalresults +'<img src="http://nextranks.com/data_images/dogs/doberman-pinscher/doberman-pinscher-03.jpg">' + '<a href="http://www.akc.org/dog-breeds/doberman-pinscher/">Doberman Pinscher</a> ' +'<p>Watchful, fearful, obedient.</p>');
-                  dogBreed = Doberman;
+                  dogBreed = 'Doberman';
                   };
 
             if (finalresults === 'ISFJ'){
                   $('#results').append( intro + finalresults +'<img src="http://www.wcrotts.com/Gallery%20Pics/A/Arzadon%20Larimbo/Arzadons%20Larimbo%2017%20months.jpg">' + '<a href="http://www.akc.org/dog-breeds/rottweiler/">Rottweiler</a> ' +'<p>Show-off and affectionate to family but reserved to strangers.</p>');
-                  dogBreed = Rottweiler;
+                  dogBreed = 'Rottweiler';
                   };
 
 
@@ -120,31 +120,29 @@ $(document).ready(function(){
             //calling the api
             $(".animalsNearYou").click(function(){
 
+            	event.preventDefault();
 
-        $.getJSON('http://api.petfinder.com/pet.find?format=json&key=ddc7ef71cbc19cde8a016e85f90c9c27&location=94103&animal=dog&breed=' + [dogBreed] + '&callback=?')
-          .done(function(petApiData) { 
-          console.log("data retrieved: ", petApiData);
-          .error(function(err) { alert('Error retrieving data!'); 
-        });
-           // 		$.getJSON('http://api.petfinder.com/pet.find?format=json&key=ddc7ef71cbc19cde8a016e85f90c9c27&location=94103&animal=dog&breed='+ [var] + 'callback=?')
-          	// 		.done(function(petApiData) { 
-          	// 			console.log("data retrieved: ", petApiData); })
-          	// 		.error(function(err) { alert('Error retrieving data!'); 
-        			// });
+        		$.getJSON('http://api.petfinder.com/pet.find?format=json&key=ddc7ef71cbc19cde8a016e85f90c9c27&location=94103&animal=dog&breed=' + [dogBreed] + '&callback=?')
+          			.done(function(petApiData) { 
+          			console.log("data retrieved: ", petApiData);
+          			$('#petfinderAnimals').text(petApiData.petfinder.pets.pet[0].name.$t);
+          			$('#petfinderAnimals').append(petApiData.petfinder.pets.pet[0].media.photos.photo[0].$t);
+          			// .error(function(err) { alert('Error retrieving data!'); 
+        		});
 
           	//append to the bottom of the html using object[#]	
-            	$("#petfinderAnimals")
-            }
+            	// $("#petfinderAnimals")
+            
 
 
-      });
+      		});
 
     
 
 
       
 
-});
+	});
       
 
 
@@ -155,4 +153,4 @@ $(document).ready(function(){
       // $("ESTJ").string.show();
 
 
-// });
+});
